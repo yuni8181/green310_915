bed07f34457c0a6c7c659df1882d124b88c99374
yuni <pinkdd@naver.com>
2023-11-07 16:29:48 +0900

    1107-3rd

--- layout/layout002.js
+++ layout/layout002.js
@@ -1,2 +1,28 @@
 // 제이쿼리선언문
 
+$(document).ready(function(){
+
+
+
+    var a =0;
+    $('.left').click(function(){
+       if(a>-2)
+        a--;
+        console.log(a);
+        $('.container ul').css('left',(60*a)+'%')
+
+
+
+
+    })
+
+    $('.right').click(function(){
+        if(a<0)
+        a++;
+        console.log(a)
+        $('.container ul').css('left',(60*a)+'%')
+
+    })
+
+
+})
